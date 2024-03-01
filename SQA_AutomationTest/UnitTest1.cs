93cf3669a1149c71401a05f2da0a78937fe236e3
Anlele1701 <lethanhduyan@gmail.com>
2024-03-01 21:29:26 +0700

    Fix bug test

--- SQA_AutomationTest/UnitTest1.cs
+++ SQA_AutomationTest/UnitTest1.cs
@@ -11,14 +11,12 @@ namespace SQA_AutomationTest
     {
         private IWebDriver driver;
         private string pathAn;
-        private Spreadsheet spreadsheet;
 
         [SetUp]
         public void Setup()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             pathAn = "C:/Users/ADMIN/Documents/HUFLIT/NAM 3/HK2/BDCL/ĐỒ ÁN/Test.xlsx";
-            spreadsheet = new Spreadsheet();
             driver = new EdgeDriver();
         }
 
@@ -28,7 +26,7 @@ namespace SQA_AutomationTest
         {
             for (int j = 0; j < parts.Length; j++)
             {
-                if (parts[j] == " ")
+                if (parts[j] == "null")
                 {
                     newString[j] = "";
                 }
@@ -36,32 +34,32 @@ namespace SQA_AutomationTest
                 {
                     newString[j] = parts[j];
                 }
-                Console.Write(newString[j]);
+                Console.WriteLine(newString[j]);
             }
         }
 
         [Test]
-        public void AutomationTest()
+        public void FUNC_CL_Login()
         {
+            Spreadsheet spreadsheet = new Spreadsheet();
             spreadsheet.LoadFromFile(@$"{pathAn}");
-            Worksheet worksheet = spreadsheet.Workbook.Worksheets.ByName($"CL - Đăng Nhập");
+            Worksheet worksheet = spreadsheet.Workbook.Worksheets.ByName("Sheet2");
             Console.WriteLine(worksheet.Name);
             int worksheetCount = worksheet.UsedRangeRowMax;
-
-            for (int i = 2; i < worksheetCount; i++)
+            for (int i = 2; i <= worksheetCount; i++)
             {
                 if (worksheet.Cell(i, 3).Merged)
                 {
                     Range objRange = worksheet.Cell(i, 3).MergedWith;
                     string mergedCellValue = Convert.ToString(worksheet.Cell(objRange.Row, objRange.LeftColumnIndex).Value);
-                    string[] parts = mergedCellValue.Split('\n');
+                    string[] parts = mergedCellValue.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
                     string[] newString = new string[parts.Length];
                     if (objRange.Row == i)
                     {
                         TestCompare(ref newString, parts);
                         driver.Navigate().GoToUrl("https://localhost:44385/Home/LoginAndRegister");
-                        driver.FindElement(By.XPath("//form[@action='/Login']//input[@id='Mail_KH']")).SendKeys(parts[0]);
-                        driver.FindElement(By.XPath("//form[@action='/Login']//input[@id='MatKhau']")).SendKeys(parts[1]);
+                        driver.FindElement(By.XPath("//form[@action='/Login']//input[@id='Mail_KH']")).SendKeys(newString[0]);
+                        driver.FindElement(By.XPath("//form[@action='/Login']//input[@id='MatKhau']")).SendKeys(newString[1]);
                         driver.FindElement(By.XPath("//button[@type='submit'][contains(text(),'Đăng Nhập')]")).Click();
                         //
                         if (driver.Url == ("https://localhost:44385") || driver.Url.Contains("Home/HomePage/2"))
@@ -79,6 +77,7 @@ namespace SQA_AutomationTest
                             catch (NoSuchElementException)
                             {
                                 Console.WriteLine("Chưa điền đủ thông tin đăng nhập");
+                                worksheet.Cell(i, 5).Value = "Chưa điền đầy đủ thông tin đăng nhập";
                             }
                         }
                     }
@@ -86,14 +85,16 @@ namespace SQA_AutomationTest
                 else
                 {
                     string cellValues = worksheet.Cell(i, 3).Value.ToString();
-                    string[] parts = cellValues.Split('\n');
+                    string[] parts = cellValues.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
                     string[] newString = new string[parts.Length];
                     TestCompare(ref newString, parts);
                     driver.Navigate().GoToUrl("https://localhost:44385/Home/LoginAndRegister");
                     driver.FindElement(By.XPath("//form[@action='/Login']//input[@id='Mail_KH']")).SendKeys(newString[0]);
                     driver.FindElement(By.XPath("//form[@action='/Login']//input[@id='MatKhau']")).SendKeys(newString[1]);
                     driver.FindElement(By.XPath("//button[@type='submit'][contains(text(),'Đăng Nhập')]")).Click();
-                    if (driver.Url.Contains("https://localhost:44385") || driver.Url.Contains("https://localhost:44385/Home/HomePage/2"))
+                    Thread.Sleep(1000);
+                    //
+                    if (driver.Url == ("https://localhost:44385") || driver.Url.Contains("https://localhost:44385/Home/HomePage/2"))
                     {
                         Console.WriteLine("Đăng nhập thành công");
                         worksheet.Cell(i, 5).Value = "Thông báo đăng nhập thành công";
@@ -107,13 +108,13 @@ namespace SQA_AutomationTest
                         }
                         catch (NoSuchElementException)
                         {
+                            worksheet.Cell(i, 5).Value = "Chưa điền đầy đủ thông tin đăng nhập";
                             Console.WriteLine("Không tìm thấy thông báo lỗi, có thể đăng nhập thành công hoặc có lỗi khác xảy ra");
                         }
                     }
                 }
             }
             Console.WriteLine("---");
-
             // Save document
             spreadsheet.SaveAs(@"C:\Users\ADMIN\Documents\HUFLIT\NAM 3\HK2\BDCL\ĐỒ ÁN\Test.xlsx");
             spreadsheet.Close();
@@ -134,7 +135,6 @@ namespace SQA_AutomationTest
         {
             driver.Quit();
             driver.Dispose();
-            spreadsheet.Dispose();
         }
     }
 }
